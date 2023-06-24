0d2ac935effbeaefe378816b4bbcdbd4091b03f2
PVmendez <valentino.mendez.rey@gmail.com>
2023-06-24 00:56:27 -0300

    fix: improve and update getProjectsData endpoint

--- src/routes/projectsRoutes.ts
+++ src/routes/projectsRoutes.ts
@@ -4,44 +4,40 @@ import Project from "../models/projectModel";
 const projectsRouter = express.Router();
 
 projectsRouter.get("/projects", async (req, res) => {
-  let highCount = 0;
-  let mediumCount = 0;
 
   try {
     const projects = await Project.find();
+
     const projectsData = projects.map((project) => {
+
+      let highCount = 0;
+      let mediumCount = 0;
+
+      project.vulnerability_list.forEach((vulnerability) => {
+        if (vulnerability.risk === "High") {
+          if (vulnerability.total) {
+            highCount += vulnerability.total;
+          }
+        } else if (vulnerability.risk === "Medium") {
+          if (vulnerability.total) {
+            mediumCount += vulnerability.total;
+          }
+        }
+      });
+    
       return {
         project_name: project.project_name,
         risk_level: project.risk_level,
         date: project.scan_date,
-      }
-    })
-
-    projects.forEach((obj) => {
-      obj.vulnerability_list.forEach((vulnerability) => {
-        if (vulnerability.risk === "High" && vulnerability.total !== undefined) {
-          highCount += vulnerability.total;
-        } else if (vulnerability.risk === "Medium" && vulnerability.total !== undefined) {
-          mediumCount += vulnerability.total;
-        }
-      });
+        levels: { high: highCount, medium: mediumCount },
+      };
     });
-    
-    const levels = {
-      high: highCount,
-      medium: mediumCount
-    };
 
-    const results = {
-      project: projectsData,
-      levels: levels,
-    }
+    console.log(projectsData);
 
-    console.log(results)
-
-    res.json(results);
+    res.json(projectsData);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: "Error getting the projects" });
   }
 });
