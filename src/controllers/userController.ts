d67d5ffda9a3fe359a19db317f5abc488bb14145
SebastianOlivera <brunos.olivera@correo.ucu.edu.uy>
2023-07-01 19:16:32 -0300

    Fix Comments

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -37,7 +37,6 @@ const userController = {
   loginUser: async (req: any, res: any) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    console.log(user)
     if (user) {
       if(user.password == password) {
         const jwtBearerToken = jwt.sign({ email }, process.env.SECRET_KEY!, {
@@ -55,21 +54,16 @@ const userController = {
   },
   registerUser: async(req: any, res: any) => {
     const { username, email, full_name, password } = req.body;
-  
     try {
       const user = await User.findOne({ email: email });
-  
       if(user) { return res.status(409).json({message:"User with this email already exists"}) }
-  
       const newUser = new User({
         email: email,
         password: password,
         username: username,
         full_name: full_name
       })
-  
       await newUser.save();
-  
       res.status(201).json({message:'User created'})
     } catch(error) {
       console.error(error);
@@ -77,23 +71,23 @@ const userController = {
     }
   },
 
-  resetPassword: async (req:any, res:any) => {
+  resetPassword: async (req: any, res: any) => {
     const { email, password, newPassword } = req.body;
     try {
       const user = await User.findOne({ email: email });
-      console.log(user)
       if (user) {
-        if (user.password == password) {
-        user.password = newPassword;
-        await user.save();
-        res.status(200).json({ message: 'Password reset successfully' });
-      }}
-      else {
+        if (user.password === password) {
+          user.password = newPassword;
+          await user.save();
+          return res.status(200).json({ message: 'Password reset successfully' });
+        } else {
+          return res.status(400).json({ message: 'Passwords do not match' });
+        }
+      } else {
         return res.status(404).json({ message: 'User Not Found' });
       }
     } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Error resetting password' });
+      return res.status(500).json({ message: 'Error resetting password' });
     }
   },
 };
